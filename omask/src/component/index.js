1a9355552bf4510207afcd5a97445e45e7cd70d1
wiz_Dan <dan@wiz-Danui-iMac.local>
2020-03-10 23:04:25 +0900

    add Markger

--- omask/src/component/index.js
+++ omask/src/component/index.js
@@ -1,11 +1,54 @@
 import React, { Component } from "react";
-import { RenderAfterNavermapsLoaded, NaverMap } from "react-naver-maps";
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.naverMaps = window.naver.maps;
+
+    this.state = {
+      center: {
+        lat: 37.42829747263545,
+        lng: 126.76620435615891
+      }
+    };
+  }
+
   componentDidMount() {
     console.log("componentDidMount");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.setState({
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            }
+          });
+        },
+        function(error) {
+          console.error(error);
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity
+        }
+      );
+    } else {
+      alert("GPS를 지원하지 않습니다");
+    }
   }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  setMap = () => {};
+
   render() {
+    console.log(111, "ddd", this.state);
     return (
       <div className="App">
         <RenderAfterNavermapsLoaded
@@ -19,9 +62,17 @@ export default class Main extends Component {
               width: "100%", // 네이버지도 가로 길이
               height: "85vh" // 네이버지도 세로 길이
             }}
-            defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
-            defaultZoom={13} // 지도 초기 확대 배율
-          />
+            center={this.state.center}
+            defaultZoom={16} // 지도 초기 확대 배율
+          >
+            <Marker
+              position={this.state.center}
+              animation={this.naverMaps.Animation.DROP}
+              onClick={() => {
+                alert("현재 위치");
+              }}
+            />
+          </NaverMap>
         </RenderAfterNavermapsLoaded>
       </div>
     );
