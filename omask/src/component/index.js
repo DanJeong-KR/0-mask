da4aa22e8cdc431b14b093f7bc0614202e3456c2
wiz_Dan <dan@wiz-Danui-iMac.local>
2020-03-14 18:38:48 +0900

    :

--- omask/src/component/index.js
+++ omask/src/component/index.js
@@ -10,6 +10,7 @@ import searchIcon from "../Images/search.svg";
 
 import moment from "moment";
 import Select from "react-select";
+import Switch from "react-switch";
 
 import {
   RenderAfterNavermapsLoaded,
@@ -29,7 +30,7 @@ export default class Main extends Component {
     this.naverMaps = window.naver.maps;
     this.mapRef = undefined;
     this.infoWindow = undefined;
-    this.current = moment(new Date()).format("MM월 D일 HH시");
+    this.day = this.getMaskDay();
 
     this.selectOptions = [
       { value: 100, label: "반경 100m" },
@@ -52,12 +53,43 @@ export default class Main extends Component {
         lat: 37.49792,
         lng: 127.02757
       },
+      isSoldoutAppear: true,
       storeDatas: [],
       radius: 1000
     };
   }
 
-  getData = async () => {
+  getMaskDay = () => {
+    const day = moment(new Date()).format("dddd");
+    switch (day) {
+      case "Monday":
+        return ["월", "1, 6"];
+      case "Tuesday":
+        return ["화", "2, 7"];
+      case "Wednesday":
+        return ["수", "3, 8"];
+      case "Thursday":
+        return ["목", "4, 9"];
+      case "Friday":
+        return ["금", "0, 5"];
+      case "Saturday":
+      case "Sunday":
+        return ["주말", "모든국민"];
+    }
+  };
+
+  componentDidMount() {
+    this.getData(this.state.isSoldoutAppear);
+    console.log("componentDidMount");
+
+    this.getCurrentPosition();
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  getData = async isSoldoutAppear => {
     this.setState({ isLoading: true }, async () => {
       const { lat, lng } = this.state.center;
       try {
@@ -76,10 +108,22 @@ export default class Main extends Component {
               isLoading: false
             });
           } else {
-            this.setState({
-              storeDatas: json.stores,
-              isLoading: false
-            });
+            if (isSoldoutAppear) {
+              this.setState({
+                storeDatas: json.stores,
+                isLoading: false
+              });
+            } else {
+              this.setState({
+                storeDatas: json.stores.filter(element => {
+                  const remain = element.remain_stat;
+                  return (
+                    remain === "few" || remain === "some" || remain === "plenty"
+                  );
+                }),
+                isLoading: false
+              });
+            }
           }
         }
       } catch (e) {
@@ -91,20 +135,6 @@ export default class Main extends Component {
       }
     });
   };
-
-  componentDidMount() {
-    this.getData();
-    console.log("componentDidMount");
-    console.log(111, "qqq", this.current);
-
-    this.getCurrentPosition();
-  }
-
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
-  }
-
-  componentWillUpdate() {}
 
   getCurrentPosition() {
     if (navigator.geolocation) {
@@ -121,7 +151,9 @@ export default class Main extends Component {
                 lng: position.coords.longitude
               }
             },
-            this.getData
+            () => {
+              this.getData(this.state.isSoldoutAppear);
+            }
           );
         },
         function(error) {
@@ -150,7 +182,9 @@ export default class Main extends Component {
           lng
         }
       },
-      this.getData
+      () => {
+        this.getData(this.state.isSoldoutAppear);
+      }
     );
   };
 
@@ -165,7 +199,9 @@ export default class Main extends Component {
       {
         radius: e.target.value
       },
-      this.getData
+      () => {
+        this.getData(this.state.isSoldoutAppear);
+      }
     );
   };
 
@@ -194,8 +230,23 @@ export default class Main extends Component {
               lng: results[0].x
             }
           },
-          this.getData
+          () => {
+            this.getData(this.state.isSoldoutAppear);
+          }
         );
+      }
+    );
+  };
+
+  onClickSoldout = checked => {
+    console.log(this.state.isSoldoutAppear);
+
+    this.setState(
+      {
+        isSoldoutAppear: checked
+      },
+      () => {
+        this.getData(this.state.isSoldoutAppear);
       }
     );
   };
@@ -227,202 +278,228 @@ export default class Main extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.isLoading && (
-          <div className="overlay">
-            <img className="loadingIcon" src={loadingIcon} alt="loadingIcon" />
-          </div>
-        )}
+        <div className="Header"> </div>
+        <div className="MapPart">
+          {this.state.isLoading && (
+            <div className="overlay">
+              <img
+                className="loadingIcon"
+                src={loadingIcon}
+                alt="loadingIcon"
+              />
+            </div>
+          )}
 
-        <div id="sidebar" className="sidebar sidebar-hide">
-          <img
-            className="sidebar_toggle"
-            src={searchIcon}
-            alt="menuIcon"
-            onClick={this.onClickSidebarToggle}
-          />
-          {/* sidebar_search */}
-          <div className="sidebar_search">
-            <DaumPostcode
-              onComplete={this.handleComplete}
-              // autoResize={true}
-              animation={true}
-              height={"100%"}
+          <RenderAfterNavermapsLoaded
+            ncpClientId={"fs0aqkdq7k"} // 자신의 네이버 계정에서 발급받은 Client ID
+            error={<p>Maps Load Error</p>}
+            loading={<p>Maps Loading...</p>}
+          >
+            <NaverMap
+              naverRef={ref => {
+                if (!this.mapRef) {
+                  this.mapRef = ref;
+                  window.naver.maps.Event.addListener(
+                    this.mapRef.instance,
+                    "dragend",
+                    this.dragendListener
+                  );
+                  window.naver.maps.Event.addListener(
+                    this.mapRef.instance,
+                    "dragstart",
+                    this.dragstartListener
+                  );
+                }
+              }}
+              mapDivId={"react-naver-map"} // default: react-naver-map
+              style={{
+                width: "100%", // 네이버지도 가로 길이
+                height: "100%" // 네이버지도 세로 길이
+              }}
+              center={this.state.center}
+              defaultZoom={16} // 지도 초기 확대 배율
+              onClick={() => {
+                if (this.infoWindow) {
+                  this.infoWindow.close();
+                }
+              }}
+            >
+              <Marker
+                position={this.state.userCenter}
+                clickable={false}
+                icon={userCenterIcon}
+                zIndex={10}
+                onClick={() => {
+                  alert(`현재 위치 입니다 ^.^`);
+                }}
+              />
+
+              {stringify(this.state.center) !==
+                stringify(this.state.userCenter) && (
+                <Marker
+                  position={this.state.center}
+                  clickable={false}
+                  icon={centerIcon}
+                  animation={this.naverMaps.Animation.BOUNCE}
+                />
+              )}
+              <Circle
+                center={this.state.center}
+                radius={this.state.radius}
+                fillOpacity={0.2}
+                fillColor={"#5DADE2"}
+                strokeColor={"#5DADE2"}
+                clickable={false}
+              />
+              {this.state.storeDatas &&
+                this.state.storeDatas.map((element, index) => {
+                  const { lat, lng, remain_stat } = element;
+                  let icon;
+                  let remain;
+                  switch (remain_stat) {
+                    case "empty":
+                      icon = nothingIcon;
+                      remain = "품절";
+                      break;
+                    case "few":
+                      icon = fewIcon;
+                      remain = "30개 미만! 방문 하셔도 품절일 수 있습니다.";
+                      break;
+                    case "some":
+                    case "plenty":
+                      icon = goodIcon;
+                      remain = "30개 이상! 방문하시는 것을 추천드립니다";
+                      break;
+                    default:
+                      icon = nothingIcon;
+                      remain = "품절";
+                      break;
+                  }
+
+                  return (
+                    <div key={index}>
+                      <Marker
+                        title={`약국 이름 : ${element.name}\n주소 : ${element.addr}`}
+                        position={{
+                          lat,
+                          lng
+                        }}
+                        icon={icon}
+                        onClick={
+                          remain_stat !== "empty"
+                            ? () => {
+                                alert(
+                                  `마스크 재고 : ${remain}\n약국 이름 : ${element.name}\n주소 : ${element.addr}`
+                                );
+                                // this.handleMarker(element);
+                              }
+                            : undefined
+                        }
+                      />
+                    </div>
+                  );
+                })}
+            </NaverMap>
+          </RenderAfterNavermapsLoaded>
+
+          {/* <div id="sidebar" className="sidebar sidebar-hide">
+            <img
+              className="sidebar_toggle"
+              src={searchIcon}
+              alt="menuIcon"
+              onClick={this.onClickSidebarToggle}
             />
-          </div>
-        </div>
-        {/* <div className="mapWrapper"> */}
 
-        {/* 오른쪽 아래 반경 선택 버튼 */}
-        {/* <Select
+            <div className="sidebar_search">
+              <DaumPostcode
+                onComplete={this.handleComplete}
+                // autoResize={true}
+                animation={true}
+                height={"100%"}
+              />
+            </div>
+          </div> */}
+          {/* <div className="mapWrapper"> */}
+
+          {/* 오른쪽 아래 반경 선택 버튼 */}
+          {/* <Select
             className="Header-select"
             value={null}
             onChange={this.handleSelectChange}
             options={this.options}
             defaultValue={500}
           /> */}
-        <select
-          className="SelectBtn"
-          onChange={this.handleSelectChange}
-          defaultValue={1000}
-        >
-          <option value={100}>반경 100m</option>
-          <option value={200}>반경 200m</option>
-          <option value={300}>반경 300m</option>
-          <option value={400}>반경 400m</option>
-          <option value={500}>반경 500m</option>
-          <option value={1000}>반경 1km</option>
-          <option value={2000}>반경 2km</option>
-          <option value={3000}>반경 3km</option>
-          <option value={4000}>반경 4km</option>
-          <option value={5000}>반경 5km</option>
-        </select>
+          <select
+            className="SelectBtn"
+            onChange={this.handleSelectChange}
+            defaultValue={1000}
+          >
+            <option value={100}>반경 100m</option>
+            <option value={200}>반경 200m</option>
+            <option value={300}>반경 300m</option>
+            <option value={400}>반경 400m</option>
+            <option value={500}>반경 500m</option>
+            <option value={1000}>반경 1km</option>
+            <option value={2000}>반경 2km</option>
+            <option value={3000}>반경 3km</option>
+            <option value={4000}>반경 4km</option>
+            <option value={5000}>반경 5km</option>
+          </select>
 
-        {/* 왼쪽 아래 내 위치 버튼 */}
-        {/* sidebar_utils */}
-        <div className="buttonWrapper" onClick={this.onClickMyPositionBtn}>
-          <img src={centerIcon} width={25} alt="focus" />
-          <p className="Header-button">내 위치</p>
-        </div>
+          {/* 왼쪽 아래 내 위치 버튼 */}
+          {/* sidebar_utils */}
+          <div className="buttonWrapper" onClick={this.onClickMyPositionBtn}>
+            <img src={centerIcon} width={25} alt="focus" />
+            <p className="Header-button">내 위치</p>
+          </div>
 
-        {/* 오른쪽 위  */}
-        {/* sidebar_markers */}
-        <div className="sidebar_markerWrapper">
-          <div className="sidebar_markers">
-            <div className="sidebar_title"></div>
-            <div className="sidebar_marker">
-              <img src={goodIcon} alt="good" />
-              <p className="sidebar_marker_desc">30개 이상!</p>
+          {/* 왼쪽 위  */}
+          {/* sidebar_markers */}
+          <div className="DailyIndicator">
+            <div className="DailyIndicator__ItemWrapper">
+              <p className="DailyIndicator__ItemWrapper--day">{this.day[0]}</p>
+              <p className="DailyIndicator__ItemWrapper--born">{this.day[1]}</p>
             </div>
-            <div className="sidebar_marker">
-              <img src={fewIcon} alt="few" />
-              <p className="sidebar_marker_desc">30개 미만!</p>
-            </div>
-            <div className="sidebar_marker">
-              <img src={nothingIcon} alt="nothingIcon" />
-              <p className="sidebar_marker_desc">품절 ㅜ ^ ㅜ</p>
-            </div>
-            <div className="sidebar_marker">
-              <img src={userCenterIcon} alt="userCenterIcon" />
-              <p className="sidebar_marker_desc">현재위치</p>
-            </div>
-            <div className="sidebar_marker">
-              <img src={centerIcon} alt="centerIcon" />
-              <p className="sidebar_marker_desc">기준 위치 (반경)</p>
+          </div>
+
+          {/* 오른쪽 위  */}
+          {/* sidebar_markers */}
+          <div className="sidebar_markerWrapper">
+            <div className="sidebar_markers">
+              <div className="sidebar_title"></div>
+              <div className="sidebar_marker">
+                <img src={goodIcon} alt="good" />
+                <p className="sidebar_marker_desc">30+</p>
+              </div>
+              <div className="sidebar_marker">
+                <img src={fewIcon} alt="few" />
+                <p className="sidebar_marker_desc">2+</p>
+              </div>
+              <div className="sidebar_marker">
+                {/* <img className="opa" src={nothingIcon} alt="nothingIcon" />
+                <p className="sidebar_marker_desc">0</p> */}
+                <Switch
+                  wit
+                  onChange={this.onClickSoldout}
+                  checked={this.state.isSoldoutAppear}
+                  onColor="#5DADE2"
+                  checkedIcon={<img src={nothingIcon} alt="nothingIcon" />}
+                  width={70}
+                  height={30}
+                />
+              </div>
+              {/* <div className="sidebar_marker">
+                <img src={userCenterIcon} alt="userCenterIcon" />
+                <p className="sidebar_marker_desc">현재위치</p>
+              </div>
+              <div className="sidebar_marker">
+                <img src={centerIcon} alt="centerIcon" />
+                <p className="sidebar_marker_desc">기준 위치 (반경)</p>
+              </div> */}
             </div>
           </div>
         </div>
-
-        <RenderAfterNavermapsLoaded
-          ncpClientId={"fs0aqkdq7k"} // 자신의 네이버 계정에서 발급받은 Client ID
-          error={<p>Maps Load Error</p>}
-          loading={<p>Maps Loading...</p>}
-        >
-          <NaverMap
-            naverRef={ref => {
-              if (!this.mapRef) {
-                this.mapRef = ref;
-                window.naver.maps.Event.addListener(
-                  this.mapRef.instance,
-                  "dragend",
-                  this.dragendListener
-                );
-                window.naver.maps.Event.addListener(
-                  this.mapRef.instance,
-                  "dragstart",
-                  this.dragstartListener
-                );
-              }
-            }}
-            mapDivId={"react-naver-map"} // default: react-naver-map
-            style={{
-              width: "100%", // 네이버지도 가로 길이
-              height: "100%" // 네이버지도 세로 길이
-            }}
-            center={this.state.center}
-            defaultZoom={16} // 지도 초기 확대 배율
-            onClick={() => {
-              if (this.infoWindow) {
-                this.infoWindow.close();
-              }
-            }}
-          >
-            <Marker
-              position={this.state.userCenter}
-              clickable={false}
-              icon={userCenterIcon}
-              zIndex={10}
-              onClick={() => {
-                alert(`현재 위치 입니다 ^.^`);
-              }}
-            />
-
-            {stringify(this.state.center) !==
-              stringify(this.state.userCenter) && (
-              <Marker
-                position={this.state.center}
-                clickable={false}
-                icon={centerIcon}
-                animation={this.naverMaps.Animation.BOUNCE}
-              />
-            )}
-            <Circle
-              center={this.state.center}
-              radius={this.state.radius}
-              fillOpacity={0.2}
-              fillColor={"#5DADE2"}
-              strokeColor={"#5DADE2"}
-              clickable={false}
-            />
-            {this.state.storeDatas &&
-              this.state.storeDatas.map((element, index) => {
-                const { lat, lng, remain_stat } = element;
-                let icon;
-                let remain;
-                switch (remain_stat) {
-                  case "empty":
-                    icon = nothingIcon;
-                    remain = "품절";
-                    break;
-                  case "few":
-                    icon = fewIcon;
-                    remain = "30개 미만! 방문 하셔도 품절일 수 있습니다.";
-                    break;
-                  case "some":
-                  case "plenty":
-                    icon = goodIcon;
-                    remain = "30개 이상! 방문하시는 것을 추천드립니다";
-                    break;
-                  default:
-                    icon = nothingIcon;
-                    remain = "품절";
-                    break;
-                }
-
-                return (
-                  <div key={index}>
-                    <Marker
-                      title={`약국 이름 : ${element.name}\n주소 : ${element.addr}`}
-                      position={{
-                        lat,
-                        lng
-                      }}
-                      icon={icon}
-                      onClick={
-                        remain_stat !== "empty"
-                          ? () => {
-                              alert(
-                                `마스크 재고 : ${remain}\n약국 이름 : ${element.name}\n주소 : ${element.addr}`
-                              );
-                              // this.handleMarker(element);
-                            }
-                          : undefined
-                      }
-                    />
-                  </div>
-                );
-              })}
-          </NaverMap>
-        </RenderAfterNavermapsLoaded>
+        {/* <div className="Footer">d</div> */}
       </div>
     );
   }
